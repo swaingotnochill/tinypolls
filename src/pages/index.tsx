e1a9e3e9df6d3ead79526bb74272bede0da2e74c
Roshan Swain <swainroshan001@gmail.com>
2022-07-29 01:14:07 +0530

    added mutation for creating questions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,22 +4,29 @@ import Image from 'next/image'
 import {prisma} from '../db/client'
 import { trpc } from '../utils/trpc'
 
+const QuestionCreator : React.FC = () => {
+  const { mutate } = trpc.useMutation("question.create")
+  return (
+    <input className='border-slate' onSubmit={(e) => {
+      console.log("Value?", e.currentTarget.value)
+    }}
+    ></input>
+  )
+}
+
 const Home: NextPage = () => {
   const {data, isLoading} = trpc.useQuery(["question.getAll"])
 
   if(isLoading || !data) return <div>Loading...</div>
-  return <div>{data[0]?.question}</div>
+  return (
+  <div>
+    <div className='flex flex-col'>
+      <div className='text-2xl font-bold'>Questions</div>
+    {data[0]?.question}
+    </div>
+    <QuestionCreator />
+  </div>
+    )
 }
 
 export default Home
-
-// Function to get all questions from the database
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany()
-
-  return {
-    props: {
-      questions: JSON.stringify(questions),
-    }
-  }
-}
