409b3cba592d07bed2082e43ebb483e5eba42216
Roshan Swain <swainroshan001@gmail.com>
2022-07-29 21:06:40 +0530

    create poll page added

---
+++ src/pages/CreatePoll.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { PlusIcon, MinusIcon } from '@heroicons/react/solid'
+
+const CreatePoll = () => {
+    return (
+        <div className='flex flex-col justify-center items-center bg-black h-screen max-h-full'>
+            <h1 className='text-4xl sm:text-6xl font-bold text-white mb-7'>Create your <span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Polls</span></h1>
+            <div className='flex justify-start items-center w-5/6 md:w-5/6 max-w-5xl'>
+                <div className='space-y-5 flex-1 p-5 rounded-md bg-white'>
+                    <div className='space-y-2'>
+                        <h1>Question</h1>
+                        <p className='flex'>
+                        <input placeholder='Add an option here...' className='flex bg-gray-200 rounded grow p-2' type='text' />
+                        </p>
+                    </div>
+                    <div className='space-y-2'>
+                        <h1>Options</h1>
+                        <p className='flex'>
+                            <input placeholder='Add an option here...' className='bg-gray-200 rounded grow p-2' type='text' />
+                            <MinusIcon className='w-10 h-10 text-black bg-gray-200 rounded ml-2 p-1 cursor-pointer'/>
+                        </p>
+                        <p className='flex'>
+                            <input placeholder='Add an option here...' className='bg-gray-200 rounded grow p-2' type='text' />
+                            <MinusIcon className='w-10 h-10 text-black bg-gray-200 rounded ml-2 p-1 cursor-pointer'/>
+                        </p>
+                    </div>
+                    <button className='bg-purple-400 text-xl font-semibold px-6 py-4 text-white rounded hover:bg-pink-600 ease-in-out duration-300 cursor-pointer'>Create Poll</button>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreatePoll
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import {prisma} from '../db/client'
 import { trpc } from '../utils/trpc'
 import React from 'react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const QuestionCreator : React.FC = () => {
   const inputRef = React.useRef<HTMLInputElement>(null)
@@ -33,7 +34,7 @@ const QuestionCreator : React.FC = () => {
 const Home: NextPage = () => {
   const {data, isLoading} = trpc.useQuery(["question.getAll"])
 
-  if(isLoading || !data) return <div>Loading...</div>
+  const router = useRouter()
 
   return (
   // <div>
@@ -54,12 +55,17 @@ const Home: NextPage = () => {
   //   <QuestionCreator />
   // </div>
 
-  <div className='flex justify-center items-center bg-black h-screen'>
-    <div className='flex flex-col justify-center items-center w-5/6 md:w-5/6 max-w-7xl space-y-7 text-white '>
+  <div className='flex justify-center items-center bg-black h-screen max-h-full'>
+    {isLoading ? (
+      <div>Loading...</div>
+    ) : (
+      <div className='flex flex-col justify-center items-center w-5/6 md:w-5/6 max-w-7xl space-y-7 text-white '>
       <h1 className='text-6xl font-bold'>Tiny<span className='font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Polls</span></h1>
       <h3 className='text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, nulla! Impedit aut magni saepe ipsam reprehenderit cumque consequuntur. Tempora exercitationem recusandae ut error adipisci quod quidem nemo ipsam quibusdam cumque.</h3>
-      <button className='bg-purple-400 text-xl font-semibold px-8 py-5 rounded hover:bg-pink-600 ease-in-out duration-300 cursor-pointer'>Create Poll</button>
+      <button onClick={()=> router.push('CreatePoll')} className='bg-purple-400 text-xl font-semibold px-8 py-5 rounded hover:bg-pink-600 ease-in-out duration-300 cursor-pointer'>Create Poll</button>
     </div>
+    )}
+
   </div>
     )
 }
