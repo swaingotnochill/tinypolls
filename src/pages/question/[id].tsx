192cc75a1321e72249c675f74d506ce7d1331f0f
Roshan Swain <swainroshan001@gmail.com>
2022-07-29 19:58:33 +0530

    fixed next build

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -10,8 +10,8 @@ const QuestionById: React.FC<{id: string }> = ({id}) => {
         <div className="p-8 flex flex-col">
             <div className="text-2xl font-bold">{data?.question}</div>
             <div>
-                {(data?.options as string[])?.map((option) => (
-                <div>{option}</div>
+                {(data?.options as string[])?.map((option,id) => (
+                <div key={id}>{option}</div>
             ))}
             </div>
         </div>
